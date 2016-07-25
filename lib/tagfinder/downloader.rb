bf60db30d10326b29e91b8aa6184f33bbcf5394d
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-07-24 17:13:55 -0700

    Provide file_creator option to Downloader

--- lib/tagfinder/downloader.rb
+++ lib/tagfinder/downloader.rb
@@ -1,10 +1,19 @@
 module Tagfinder
   class Downloader
-    include Procto.call, Concord.new(:url, :local_path)
+    include Procto.call, Concord.new(:url, :local_path, :file_creator)
+
+    def initialize(url, local_path, file_creator: TmpFileCreator)
+      super(url, local_path, file_creator)
+    end
 
     def call
-      file_body = Connection.call(Request.new(url))
-      TmpFileCreator.call(local_path, file_body)
+      file_creator.call(local_path, file_body)
+    end
+
+    private
+
+    def file_body
+      Connection.call(Request.new(url))
     end
   end
 end
